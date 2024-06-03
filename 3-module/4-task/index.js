0d9964b070e656473fb1dafc10c029d3d24eabaf
Vladimir Khleborodov <khleborodov@hotmail.com>
2024-06-03 21:50:26 +0300

    В 4 задаче использован метод map

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,11 +1,6 @@
 function showSalary(users, age) {
-  let result = [];
-  const filteredUsers = users.filter(user => user.age <= age);
-  
-  for (const user of filteredUsers) {
-    const {balance, name} = user;
-    result.push(`${name}, ${balance}`);
-  }
-  
-  return result.join('\n');
+  return users
+    .filter(user => user.age <= age)
+    .map(user => `${user.name}, ${user.balance}`)
+    .join('\n');
 }
