308113516f8976629c695e76bf7fb88450b55265
Vladimir Khleborodov <khleborodov@hotmail.com>
2024-06-10 19:55:38 +0300

    Решены все задачи 5 лекции

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector('.carousel');
+  const carouselArrowRight = carousel.querySelector('.carousel__arrow_right');
+  const carouselArrowLeft = carousel.querySelector('.carousel__arrow_left');
+  const carouselInner = carousel.querySelector('.carousel__inner');
+  const carouselSlidesArr = Array.from(carouselInner.children);
+  
+  carouselArrowLeft.style.display = 'none';
+  let counter = 0;
+  let dynamicSlideWidth = 0;
+  
+  carousel.addEventListener('click', (event) => {
+    let target = event.target;
+    
+    if (target.closest('.carousel__arrow_right')) {
+      counter++;
+      if (counter === carouselSlidesArr.length - 1) carouselArrowRight.style.display = 'none';
+      
+      carouselArrowLeft.style.display = '';
+      dynamicSlideWidth += carouselSlidesArr[counter].offsetWidth;
+      carouselInner.style.transform = `translateX(-${dynamicSlideWidth}px)`;
+    }
+    
+    if (target.closest('.carousel__arrow_left')) {
+      counter--;
+      if (counter === 0) carouselArrowLeft.style.display = 'none';
+      
+      carouselArrowRight.style.display = '';
+      dynamicSlideWidth -= carouselSlidesArr[counter].offsetWidth;
+      carouselInner.style.transform = `translateX(-${dynamicSlideWidth}px)`;
+    }
+  });
 }
